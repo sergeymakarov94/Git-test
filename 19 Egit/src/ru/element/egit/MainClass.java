0d1a6c7d0107f091b3e4906828f858d1c6da05f4
Makarov Sergey <spek94@list.ru>
2015-03-28 14:23:12 +0300

    UPDATE 2

--- 19 Egit/src/ru/element/egit/MainClass.java
+++ 19 Egit/src/ru/element/egit/MainClass.java
@@ -5,5 +5,6 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Hello from Github!!!");
 		System.out.println("UPDATE!!!");
+		System.out.println("UPDATE 2!!!");
 	}
 }
