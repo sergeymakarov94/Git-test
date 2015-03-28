03e419b7baf5ed5b28e9576bdfd26c3b76805c41
Makarov Sergey <spek94@list.ru>
2015-03-28 15:19:31 +0300

    18

---
+++ 18 Serialization/src/ru/element/serialization/MainClass.java
@@ -0,0 +1,68 @@
+package ru.element.serialization;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import javax.imageio.IIOException;
+import javax.swing.JOptionPane;
+
+public class MainClass {
+	private static ArrayList<Profile> profiles = new ArrayList<Profile>();
+
+	public static void main(String[] args) {
+		profiles = (ArrayList<Profile>) deserData("profiles");
+		System.out.println(profiles.size());
+		Profile profile = new Profile();
+		profile.setName(JOptionPane.showInputDialog(null, "¬ведите ваше им€"));
+		profile.setSurname(JOptionPane.showInputDialog(null,
+				"¬ведите вашу фамилию"));
+		profiles.add(profile);
+		for (Profile p : profiles) {
+			System.out.println(p.getName() + " " + p.getSurname());
+		}
+		System.out.println(profiles.size());
+		serData("profiles", profiles);
+	}
+
+	private static Object deserData(String file_name) {
+		Object retObject = null;
+		try {
+			FileInputStream fileIn = new FileInputStream(file_name + ".ser");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			retObject = in.readObject();
+			fileIn.close();
+			in.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Can't find file");
+			System.exit(1);
+		} catch (IOException e) {
+			System.out.println("Input/Output error");
+			System.exit(2);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Can't find class");
+			System.exit(3);
+		}
+		return retObject;
+	}
+
+	private static void serData(String file_name, Object obj) {
+		try {
+			FileOutputStream fileOut = new FileOutputStream(file_name + ".ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(obj);
+			fileOut.close();
+			out.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Can't find file");
+			System.exit(1);
+		} catch (IOException e) {
+			System.out.println("Input/Output error");
+			System.exit(2);
+		}
+	}
+}
